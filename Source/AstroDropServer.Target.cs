615b8ab69eb92b0795ff03db2df206a390e8f1e2
Luiz Torini <luiz.torini@hotmail.com>
2026-06-21 11:32:32 -0300

    Updade Version Unreal 5.8

--- Source/AstroDropServer.Target.cs
+++ Source/AstroDropServer.Target.cs
@@ -8,8 +8,8 @@ public class AstroDropServerTarget : TargetRules
 	public AstroDropServerTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Server;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 		ExtraModuleNames.Add("AstroDrop");
 	}
 }
