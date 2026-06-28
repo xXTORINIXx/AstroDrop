388248fe6e25d2212cf34cbb0a773da45557ace7
Luiz Torini <luiz.torini@hotmail.com>
2026-06-28 11:52:21 -0300

    Projectile Weapon Class

---
+++ Source/AstroDrop/Public/Weapon/ProjectileWeapon.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon/Weapon.h"
+#include "ProjectileWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASTRODROP_API AProjectileWeapon : public AWeapon
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
