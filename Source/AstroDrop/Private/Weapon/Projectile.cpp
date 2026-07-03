046e4d66ccca9afd25de2285d41b23a69aec3bc0
Luiz Torini <luiz.torini@hotmail.com>
2026-07-03 16:37:43 -0300

    Projectile Tracer

--- Source/AstroDrop/Private/Weapon/Projectile.cpp
+++ Source/AstroDrop/Private/Weapon/Projectile.cpp
@@ -4,12 +4,14 @@
 #include "Weapon/Projectile.h"
 #include "Components/BoxComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 
 AProjectile::AProjectile()
 {
 	
 	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
 
 	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
 	SetRootComponent(CollisionBox);
@@ -27,6 +29,18 @@ AProjectile::AProjectile()
 void AProjectile::BeginPlay()
 {
 	Super::BeginPlay();
+	
+	if (Tracer)
+	{
+		TracerComponent = UGameplayStatics::SpawnEmitterAttached(
+			Tracer,
+			CollisionBox,
+			FName(),
+			GetActorLocation(),
+			GetActorRotation(),
+			EAttachLocation::KeepWorldPosition
+		);
+	}
 	
 }
 
